dfa15b517013ea07ab69f7728a877d96cd1b6079
Kshitijaa Jaglan <29124655+KshitijaaJaglan@users.noreply.github.com>
2018-03-24 21:04:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,166 @@
+window.onload = function(){
+
+
+document.getElementById("myBtn-01").onclick = function() {
+    document.getElementById('myModal-01').style.display = "block";
+}
+document.getElementsByClassName("cl1")[0].onclick = function() {
+    document.getElementById('myModal-01').style.display = "none";
+}
+
+document.getElementById("myBtn-02").onclick = function() {
+    document.getElementById('myModal-02').style.display = "block";
+}
+document.getElementsByClassName("cl2")[0].onclick = function() {
+    document.getElementById('myModal-02').style.display = "none";
+}
+
+document.getElementById("myBtn-03").onclick = function() {
+   document.getElementById('myModal-03').style.display = "block";
+}
+document.getElementsByClassName("cl3")[0].onclick = function() {
+   document.getElementById('myModal-03').style.display = "none";
+}
+
+document.getElementById("myBtn-04").onclick = function() {
+    document.getElementById('myModal-04').style.display = "block";
+}
+document.getElementsByClassName("cl4")[0].onclick = function() {
+   document.getElementById('myModal-04').style.display = "none";
+}
+
+document.getElementById("myBtn-05").onclick = function() {
+    document.getElementById('myModal-05').style.display = "block";
+}
+document.getElementsByClassName("cl5")[0].onclick = function() {
+   document.getElementById('myModal-05').style.display = "none";
+}
+
+document.getElementById("myBtn-06").onclick = function() {
+    document.getElementById('myModal-06').style.display = "block";
+}
+document.getElementsByClassName("cl6")[0].onclick = function() {
+    document.getElementById('myModal-06').style.display = "none";
+}
+
+document.getElementById("myBtn-07").onclick = function() {
+    document.getElementById('myModal-07').style.display = "block";
+}
+document.getElementsByClassName("cl7")[0].onclick = function() {
+    document.getElementById('myModal-07').style.display = "none";
+}
+
+document.getElementById("myBtn-08").onclick = function() {
+    document.getElementById('myModal-08').style.display = "block";
+}
+document.getElementsByClassName("cl8")[0].onclick = function() {
+    document.getElementById('myModal-08').style.display = "none";
+}
+
+document.getElementById("myBtn-09").onclick = function() {
+    document.getElementById('myModal-09').style.display = "block";
+}
+document.getElementsByClassName("cl9")[0].onclick = function() {
+    document.getElementById('myModal-09').style.display = "none";
+}
+
+document.getElementById("myBtn-10").onclick = function() {
+    document.getElementById('myModal-10').style.display = "block";
+}
+document.getElementsByClassName("cl10")[0].onclick = function() {
+    document.getElementById('myModal-10').style.display = "none";
+}
+
+document.getElementById("myBtn-11").onclick = function() {
+    document.getElementById('myModal-11').style.display = "block";
+}
+document.getElementsByClassName("cl11")[0].onclick = function() {
+    document.getElementById('myModal-11').style.display = "none";
+}
+
+document.getElementById("myBtn-12").onclick = function() {
+    document.getElementById('myModal-12').style.display = "block";
+}
+document.getElementsByClassName("cl12")[0].onclick = function() {
+    document.getElementById('myModal-12').style.display = "none";
+}
+
+document.getElementById("myBtn-13").onclick = function() {
+    document.getElementById('myModal-13').style.display = "block";
+}
+document.getElementsByClassName("cl13")[0].onclick = function() {
+    document.getElementById('myModal-13').style.display = "none";
+}
+
+document.getElementById("myBtn-14").onclick = function() {
+    document.getElementById('myModal-14').style.display = "block";
+}
+document.getElementsByClassName("cl14")[0].onclick = function() {
+    document.getElementById('myModal-14').style.display = "none";
+}
+
+document.getElementById("myBtn-15").onclick = function() {
+    document.getElementById('myModal-15').style.display = "block";
+}
+document.getElementsByClassName("cl15")[0].onclick = function() {
+    document.getElementById('myModal-15').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal-01')) {
+       document.getElementById('myModal-01').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-02')) {
+       document.getElementById('myModal-02').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-03')) {
+       document.getElementById('myModal-03').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-04')) {
+       document.getElementById('myModal-04').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-05')) {
+       document.getElementById('myModal-05').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-06')) {
+        document.getElementById('myModal-06').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-07')) {
+       document.getElementById('myModal-07').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-08')) {
+       document.getElementById('myModal-08').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-09')) {
+       document.getElementById('myModal-09').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-10')) {
+       document.getElementById('myModal-10').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-11')) {
+       document.getElementById('myModal-11').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-12')) {
+       document.getElementById('myModal-12').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-13')) {
+       document.getElementById('myModal-13').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-14')) {
+       document.getElementById('myModal-14').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-15')) {
+       document.getElementById('myModal-15').style.display = "none";
+    }
+}
+
+
+}
