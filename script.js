1d97c09d40bfd6054f6b10b6cc2f327e69162dce
Kshitijaa Jaglan <29124655+KshitijaaJaglan@users.noreply.github.com>
2018-03-25 20:30:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
 window.onload = function(){
 
 
-document.getElementById("myBtn-01").onclick = function() {
+document.getElementById("hv-1").onclick = function() {
     document.getElementById('myModal-01').style.display = "block";
 }
 document.getElementsByClassName("cl1")[0].onclick = function() {
     document.getElementById('myModal-01').style.display = "none";
 }
 
-document.getElementById("myBtn-02").onclick = function() {
+document.getElementById("hv-2").onclick = function() {
     document.getElementById('myModal-02').style.display = "block";
 }
 document.getElementsByClassName("cl2")[0].onclick = function() {
     document.getElementById('myModal-02').style.display = "none";
 }
 
-document.getElementById("myBtn-03").onclick = function() {
+document.getElementById("hv-3").onclick = function() {
    document.getElementById('myModal-03').style.display = "block";
 }
 document.getElementsByClassName("cl3")[0].onclick = function() {
    document.getElementById('myModal-03').style.display = "none";
 }
 
-document.getElementById("myBtn-04").onclick = function() {
+document.getElementById("hv-4").onclick = function() {
     document.getElementById('myModal-04').style.display = "block";
 }
 document.getElementsByClassName("cl4")[0].onclick = function() {
    document.getElementById('myModal-04').style.display = "none";
 }
 
-document.getElementById("myBtn-05").onclick = function() {
+document.getElementById("hv-5").onclick = function() {
     document.getElementById('myModal-05').style.display = "block";
 }
 document.getElementsByClassName("cl5")[0].onclick = function() {
    document.getElementById('myModal-05').style.display = "none";
 }
 
-document.getElementById("myBtn-06").onclick = function() {
+document.getElementById("hv-6").onclick = function() {
     document.getElementById('myModal-06').style.display = "block";
 }
 document.getElementsByClassName("cl6")[0].onclick = function() {
     document.getElementById('myModal-06').style.display = "none";
 }
 
-document.getElementById("myBtn-07").onclick = function() {
+document.getElementById("hv-7").onclick = function() {
     document.getElementById('myModal-07').style.display = "block";
 }
 document.getElementsByClassName("cl7")[0].onclick = function() {
     document.getElementById('myModal-07').style.display = "none";
 }
 
-document.getElementById("myBtn-08").onclick = function() {
+document.getElementById("hv-8").onclick = function() {
     document.getElementById('myModal-08').style.display = "block";
 }
 document.getElementsByClassName("cl8")[0].onclick = function() {
     document.getElementById('myModal-08').style.display = "none";
 }
 
-document.getElementById("myBtn-09").onclick = function() {
+document.getElementById("hv-9").onclick = function() {
     document.getElementById('myModal-09').style.display = "block";
 }
 document.getElementsByClassName("cl9")[0].onclick = function() {
     document.getElementById('myModal-09').style.display = "none";
 }
 
-document.getElementById("myBtn-10").onclick = function() {
+document.getElementById("hv-10").onclick = function() {
     document.getElementById('myModal-10').style.display = "block";
 }
 document.getElementsByClassName("cl10")[0].onclick = function() {
     document.getElementById('myModal-10').style.display = "none";
 }
 
-document.getElementById("myBtn-11").onclick = function() {
+document.getElementById("hv-11").onclick = function() {
     document.getElementById('myModal-11').style.display = "block";
 }
 document.getElementsByClassName("cl11")[0].onclick = function() {
     document.getElementById('myModal-11').style.display = "none";
 }
 
-document.getElementById("myBtn-12").onclick = function() {
+document.getElementById("hv-12").onclick = function() {
     document.getElementById('myModal-12').style.display = "block";
 }
 document.getElementsByClassName("cl12")[0].onclick = function() {
     document.getElementById('myModal-12').style.display = "none";
 }
 
-document.getElementById("myBtn-13").onclick = function() {
+document.getElementById("hv-13").onclick = function() {
     document.getElementById('myModal-13').style.display = "block";
 }
 document.getElementsByClassName("cl13")[0].onclick = function() {
     document.getElementById('myModal-13').style.display = "none";
 }
 
-document.getElementById("myBtn-14").onclick = function() {
+document.getElementById("hv-14").onclick = function() {
     document.getElementById('myModal-14').style.display = "block";
 }
 document.getElementsByClassName("cl14")[0].onclick = function() {
     document.getElementById('myModal-14').style.display = "none";
 }
 
-document.getElementById("myBtn-15").onclick = function() {
+document.getElementById("hv-15").onclick = function() {
     document.getElementById('myModal-15').style.display = "block";
 }
 document.getElementsByClassName("cl15")[0].onclick = function() {
@@ -162,5 +162,9 @@ window.onclick = function(event) {
     }
 }
 
+/*
+document.getElementById("myBtn-01").onmouseover = function() {
+    document.getElementById('hover-content-01').style.display = "block";
+}*/
 
 }
